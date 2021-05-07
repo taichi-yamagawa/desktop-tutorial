d21563fb6be2c7e8de08e1710fbf9184a26b8c51
taichi-yamagawa <taichiclef@gmail.com>
2021-05-07 15:46:15 +0900

    課題提出_Java_Task1-2_修正
    Task1_2 .javaの修正

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -1,3 +1,4 @@
+ 
 /**
  * Task1-2 : 課題内容
  *
@@ -10,33 +11,32 @@
  */
 public class Task1_2 {
 
-    public static void main(String[] args) {
-
-        // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
-        String[] country = {"JAPAN","AMERICA","KOREA"};
-
-        // ② ①で作成した配列の要素数を出力してください。
-        System.out.println(country.length);
-
-        /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
-         *   1番目（先頭）の要素に 「りんご」 を代入
-         *   2番目の要素に 「もも」 を代入
-         *   3番目の要素に 「ぶどう」 を代入
-         */
-        String[] strArray = {"りんご","もも","ぶどう"};
-
-        // ④ ③で作成した配列の2番目の要素を出力しなさい。
-        System.out.println(strArray[1]);
-
-        /* ⑤下記の処理について、何をしているのかコメントを記入してください。
-         *  要素数が5のint型の配列 intArray を定義
-         */
-        int[] intArray = { 10, 20, 30, 40, 50 };
-
-        // ⑥下記の処理について、何をしているのかコメントを記入してください。
-        // int型配列 intArray の1番目の要素と4番目の要素を足す
-        // int型配列 intArray の2番目の要素と5番目の要素を足して出力する
-        System.out.println(intArray[1] + intArray[4]);
-
-    }
-}
+        public static void main(String[] args) {
+    
+            // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
+            String[] country = {"JAPAN","AMERICA","KOREA"};
+    
+            // ② ①で作成した配列の要素数を出力してください。
+            System.out.println(country.length);
+    
+            /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
+             *   1番目（先頭）の要素に 「りんご」 を代入
+             *   2番目の要素に 「もも」 を代入
+             *   3番目の要素に 「ぶどう」 を代入
+             */
+            String[] strArray = {"りんご","もも","ぶどう"};
+    
+            // ④ ③で作成した配列の2番目の要素を出力しなさい。
+            System.out.println(strArray[1]);
+    
+            /* ⑤下記の処理について、何をしているのかコメントを記入してください。
+             *  要素数が5のint型の配列 intArray を定義
+             */
+            int[] intArray = { 10, 20, 30, 40, 50 };
+    
+            // ⑥下記の処理について、何をしているのかコメントを記入してください。
+            // int型配列 intArray の2番目の要素と5番目の要素を足して出力する
+            System.out.println(intArray[1] + intArray[4]);
+    
+        }
+    } 
