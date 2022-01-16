3230b11139e7c946f76b9ec69c1410885b3c23d1
taichi-yamagawa <taichiclef@gmail.com>
2022-01-16 23:07:20 +0900

    課題提出_CCNA_3章
    IPv6 -関連用語-

---
+++ IPv6.java
@@ -0,0 +1,17 @@
+/*
+
+
+--問--
+以下のMACアドレスを確認し、EUI-64を用いて64ビットのインターフェースIDに拡張してください。
+
+　　　（１）MACアドレス：AAAA.AAAA.AAAA
+	 インターフェースID：A8 AA AA FF FE AA AA AA
+
+　　　（２）MACアドレス：9876.5432.10FE
+	 インターフェースID：9A 76 54 FF FE 32 10 FE
+
+　　　（３）MACアドレス：FFFF.FFFF.FFFF
+	 インターフェースID：FD FF FF FF FE FF FF FF
+
+	 
+*/
